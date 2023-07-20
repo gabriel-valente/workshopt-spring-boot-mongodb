4056e014a5859f40898ee8b62d99690eb95da77f
Gabriel Valente <gabrielmcv1@gmail.com>
2023-07-21 00:10:43 +0100

    MongoDB Connection, Repository and Service

--- src/main/java/com/muhdo/workshopmongo/domain/User.java
+++ src/main/java/com/muhdo/workshopmongo/domain/User.java
@@ -1,8 +1,13 @@
 package com.muhdo.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 
+@Document
 public class User implements Serializable {
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/muhdo/workshopmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package com.muhdo.workshopmongo.services;
+
+import com.muhdo.workshopmongo.domain.User;
+import com.muhdo.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
