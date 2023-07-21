b20313945c46c15e96e7ece6dd615b034bc4e61f
Gabriel Valente <gabrielmcv1@gmail.com>
2023-07-21 18:38:31 +0100

    Insert User

--- src/main/java/com/muhdo/workshopmongo/services/UserService.java
+++ src/main/java/com/muhdo/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.muhdo.workshopmongo.services;
 
 import com.muhdo.workshopmongo.domain.User;
+import com.muhdo.workshopmongo.dto.UserDTO;
 import com.muhdo.workshopmongo.repositories.UserRepository;
 import com.muhdo.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
