37d69a73ca5a51c057bde56ca0c79af713ff3e87
Gabriel Valente <gabrielmcv1@gmail.com>
2023-07-22 18:12:51 +0100

    Search Posts by Title

--- src/main/java/com/muhdo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/muhdo/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.muhdo.workshopmongo.resources;
 
 import com.muhdo.workshopmongo.domain.Post;
+import com.muhdo.workshopmongo.resources.util.URL;
 import com.muhdo.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -13,9 +16,16 @@ public class PostResource {
     private PostService service;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Post> findAll(@PathVariable String id) {
+    public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        List<Post> list = service.findByTitle(URL.decodeParam(text));
+
+        return ResponseEntity.ok().body(list);
     }
 }
